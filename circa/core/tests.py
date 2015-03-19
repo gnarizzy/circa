55e2e561fce023f209e358bc8dbca31e24ed1914
Gautam <gautam@uga.edu>
2015-03-19 12:32:43 -0400

    Wrote test to check that homepage resolves correctly

--- circa/core/tests.py
+++ circa/core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from core.views import index
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
