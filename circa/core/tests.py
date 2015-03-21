f1ddad56de91ee3ff41c4f55d46bde3ff164f2cb
Gautam <gautam@uga.edu>
2015-03-21 15:11:38 -0400

    Added test for minimal HTML in index view

--- circa/core/tests.py
+++ circa/core/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from core.views import index
 
 
@@ -8,3 +10,10 @@ class HomePageTests(TestCase):
     def test_root_url_resolves_to_index_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Circa - Buy and Sell Locally</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
