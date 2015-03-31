79b2cd60f01998cb20a0f8c10e61d7a05a47142e
Gautam <gautam@uga.edu>
2015-03-30 23:52:50 -0400

    Started sell view

--- circa/core/views.py
+++ circa/core/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from core.models import Item
-# Create your views here.
+
+#home page
 def index(request):
     item_list = Item.objects.all()
     context = {'items':item_list}
     return render(request, 'index.html', context)
+
+#posting an item
+def sell(request):
+    context = {}
+    return render(request,'sell.html',context)
 
 def todo(request):
     return render(request,'todo.html')
