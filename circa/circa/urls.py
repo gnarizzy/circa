dffb3e69b03c1b4fb30da5df4d9e4e5f2b19da12
Gautam <gautam@uga.edu>
2015-03-30 23:54:02 -0400

    url mapping for sell view

--- circa/circa/urls.py
+++ circa/circa/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^todo/','core.views.todo', name= 'todo'),
+    url(r'^sell/','core.views.sell', name= 'sell'),
     url(r'^$', 'core.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
