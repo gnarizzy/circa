772cd55fca0f22ee5ee999c966b4f38b24741a4b
Gautam <gautam@uga.edu>
2015-04-01 16:40:57 -0400

    Updated auction model

--- circa/core/models.py
+++ circa/core/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 class Auction(models.Model):
+    SHORT = 1
+    MEDIUM = 3
+    LONG = 5
+    DURATION_CHOICES = ((SHORT, '1 day'),(MEDIUM, '3 days'), (LONG, '5 days'),)
+    starting_bid = models.DecimalField(max_digits = 6, decimal_places = 2, default=1.00)
     current_bid = models.DecimalField(max_digits = 6, decimal_places = 2, default=1.00)
+    buy_now_price = models.DecimalField(max_digits = 6, decimal_places = 2, default = 1.10)
+    start_date = models.DateTimeField(auto_now_add = True, default = datetime.datetime.now())
+    duration = models.IntegerField(choices = DURATION_CHOICES, default = MEDIUM)
+    end_date = models.DateTimeField(default = datetime.datetime.now())
+    current_bidder = models.OneToOneField(User, null = True)
 
 class Item(models.Model):
     title = models.TextField(default='')
     description = models.TextField(default='')
-    photo1 = models.URLField()
-    photo2 = models.URLField()
-    photo3 = models.URLField()
+    photo1 = models.URLField(null = True)
+    photo2 = models.URLField(null = True)
+    photo3 = models.URLField(null = True)
     auction = models.OneToOneField(Auction, null = True)
     seller = models.OneToOneField(User, related_name ='seller_profile', null = True)
     buyer = models.OneToOneField(User, related_name = 'buyer_profile', null = True)
