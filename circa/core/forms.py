640935b7132e77b707501d537f4bb1dedb91d08c
Gautam <gautam@uga.edu>
2015-04-01 16:41:54 -0400

    Added auction form

--- circa/core/forms.py
+++ circa/core/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
-from core.models import Item
+from core.models import Item, Auction
 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('title', 'description','photo1','photo2','photo3',)
+
+class AuctionForm(forms.ModelForm):
+    starting_bid = forms.DecimalField(label = "Starting bid")
+    buy_now_price = forms.DecimalField(label = "Buy now price")
+    class Meta:
+        model = Auction
+        fields = ('starting_bid', 'buy_now_price', 'duration')
