56c08e16717b54883e470e0315820d08dbd4917f
Gautam <gautam@uga.edu>
2015-04-01 04:30:23 -0400

    Parameterized auction urls

--- circa/circa/urls.py
+++ circa/circa/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^todo/','core.views.todo', name= 'todo'),
     url(r'^sell/','core.views.sell', name= 'sell'),
-    url(r'^auction/','core.views.acution',name= 'auction'),
+    url(r'^auction/(?P<itemid>\d+)/$','core.views.auction',name= 'auction'),
     url(r'^$', 'core.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
