ab6462d6fcbc5cf4ceae456641078cc8ae4dca8c
Gautam <gautam@uga.edu>
2015-04-01 00:39:03 -0400

    Auction url

--- circa/circa/urls.py
+++ circa/circa/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^todo/','core.views.todo', name= 'todo'),
     url(r'^sell/','core.views.sell', name= 'sell'),
+    url(r'^auction/','core.views.acution',name= 'auction'),
     url(r'^$', 'core.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
