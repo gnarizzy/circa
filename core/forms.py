7ca7bf466ac00db643fb9bcbe63fdac7194e4c3c
Gautam <gautam@uga.edu>
2015-04-02 00:33:25 -0400

    Form validation now checks for starting bids < $1 and too low buy it nows

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from decimal import *
 
 from core.models import Item, Auction
 
@@ -11,17 +12,21 @@ class AuctionForm(forms.ModelForm):
     starting_bid = forms.DecimalField(label = "Starting bid")
     buy_now_price = forms.DecimalField(label = "Buy now price")
 
+#Make sure starting bid is at least $1.00
     def clean_starting_bid(self):
         starting_bid = self.cleaned_data['starting_bid']
         if starting_bid < 1:
             raise forms.ValidationError("The minimum starting bid is $1.00.")
         return starting_bid
 
+#Make sure buy now price is at least 10% greater than starting bid
     def clean_buy_now_price(self):
         starting_bid = self.cleaned_data['starting_bid']
         buy_now_price = self.cleaned_data['buy_now_price']
-        if starting_bid * 1.10 < buy_now_price:
-            raise forms.ValidationError("Buy now price must be at least 10% higher than starting bid.")
+
+        if starting_bid * Decimal(1.10)> buy_now_price:
+            raise forms.ValidationError("Buy now price must be more than 10% higher than starting bid.")
+
         return buy_now_price
 
     class Meta:
