0f5518e81c1097200b117f239db5c9ae97ac9e1e
Gautam <gautam@uga.edu>
2015-04-05 03:49:06 -0400

    Added auction detail url

--- circa/circa/urls.py
+++ circa/circa/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     url(r'^todo/','core.views.todo', name= 'todo'),
     url(r'^sell/','core.views.sell', name= 'sell'),
     url(r'^createauction/(?P<itemid>\d+)/$','core.views.create_auction',name= 'create_auction'),
+    url(r'^auction/(?P<auctionid>\d+)/$','core.views.auction_detail',name= 'auction_detail'),
     url(r'^$', 'core.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
