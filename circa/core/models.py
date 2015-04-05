b8b077f36f1c3d34a3f57d0ccedf8a6f9514055d
Gautam <gautam@uga.edu>
2015-04-05 03:49:26 -0400

    Removed unnecessary imports

--- circa/core/models.py
+++ circa/core/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
-import datetime
 # Create your models here.
 
 class Auction(models.Model):
