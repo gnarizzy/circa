4896269a1bc0a8393e878dec078c0a264a111388
Gautam <gautam@uga.edu>
2015-04-15 04:10:17 -0400

    Item buyer is now ManyToOne (many auctions, one bidder)

--- circa/core/models.py
+++ circa/core/models.py
@@ -28,7 +28,7 @@ class Item(models.Model):
     #photo3 = models.URLField(null = True)
     auction = models.OneToOneField(Auction, null = True)
     seller = models.ForeignKey(User, related_name ='seller_profile', null = True) #many items per one seller
-    buyer = models.OneToOneField(User, related_name = 'buyer_profile', null = True)
+    buyer = models.ForeignKey(User, related_name = 'buyer_profile', null = True)
 
     def __str__(self):
         return self.title
