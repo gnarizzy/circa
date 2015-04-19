6fcf46daf874aa51c4dd1bdee02b34a2ab496dcc
Gautam <gautam@uga.edu>
2015-04-18 23:20:10 -0400

    Admin now includes cropping stuff

--- circa/core/admin.py
+++ circa/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from core.models import Item, Auction, UserProfile
+from image_cropping import ImageCroppingMixin
 
-admin.site.register(Item)
+class ItemAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Auction)
 admin.site.register(UserProfile)
