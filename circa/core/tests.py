079f808fd8daf03efe03997d580e67370e4e0004
Gautam <gautam@uga.edu>
2015-04-24 03:18:21 -0400

    test notes for help and success pages

--- circa/core/tests.py
+++ circa/core/tests.py
@@ -226,3 +226,5 @@ class CreateAuctionTest(TestCase):
 
 #Test all the stripe stuff, from json to template and all the tests stripe lists on their site, test declined cards,
 #test creating users, test notifications after purchase is made
+
+#test success page, help page
