753415021a5deb44b9e60344799fec90a9ec69b3
Gautam <gautam@uga.edu>
2015-05-03 02:51:52 -0400

    notes on ending bid flow

--- circa/core/tests.py
+++ circa/core/tests.py
@@ -237,3 +237,5 @@ class CreateAuctionTest(TestCase):
 
 #create deliveries, delivery object
 
+#payment flow test (pay rendersw correct template, requires login, shows correct pending payments [both one or multiple]
+#and allows payment) 
