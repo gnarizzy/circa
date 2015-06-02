8586117d666e3a3eb2a8e1e1e0522daa7110e9c4
Andrew <aschuster3@gatech.edu>
2015-06-02 16:31:03 -0400

    Reverted certain error catching mechanisims

--- circa/core/tasks.py
+++ circa/core/tasks.py
@@ -1,18 +1,8 @@
 from background_task import background
 from core.models import User, Listing
-from core.email import offer_accepted_notification
-from circa.settings import COMMISSION_FLAT, COMMISSION_PERCENT, COMMISSION_BREAKEVEN
-from decimal import *
-from django.core.mail import EmailMessage
+from core.email import offer_accepted_notification, offer_accepted_seller_notification
 
 import datetime
-
-OFFER_OVER = "Dear {},\n\nYour item, {}, has a winning offer!  Your total earnings are predicted to be ${}, which " \
-             "will be given to you upon pick up of the item.  Please reply to this email with your address and " \
-             "preferred pick-up time.\n\nThank you for using Circa!  If you have any feedback on your " \
-             "experience, please reply to this email and let us know.  We want to be the best way for students to buy " \
-             "and sell electronics locally." \
-             "\n\nSincerely\n\nThe Circa Team"
 
 @background(schedule=datetime.timedelta(hours=1))
 def queue_for_email_notifications(user_id, listing_id):
@@ -23,24 +13,3 @@ def queue_for_email_notifications(user_id, listing_id):
 
         offer_accepted_seller_notification(listing)
     return
-
-def offer_accepted_seller_notification(listing):
-    price = listing.current_offer
-    if price <= COMMISSION_BREAKEVEN:
-        earnings = price - Decimal(COMMISSION_FLAT)
-    else:
-        earnings = price * Decimal(1 - COMMISSION_PERCENT)
-
-    content = OFFER_OVER.format(listing.item.seller.username, listing.item.title, earnings)
-
-    recipient = list()
-    recipient.append(listing.item.seller.email)
-
-    message = EmailMessage(
-        subject="{} has a winning offer!".format(listing.item.title),
-        body=content,
-        to=recipient
-    )
-    message.send()
-
-    return message.mandrill_response[0]
