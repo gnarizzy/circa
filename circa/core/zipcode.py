d0b64010bda7aa598d0107273ef13015fb23a324
Andrew <aschuster3@gatech.edu>
2015-06-07 17:56:29 -0400

    Zipcode updated, about page questions moved to help page

--- circa/core/zipcode.py
+++ circa/core/zipcode.py
@@ -1,5 +1,5 @@
 # simple script that returns all the zip codes we serve.
 
 def zipcodes():
-    zips = [30309,30318,30313,30308,30332]
+    zips = [30309, 30318, 30313, 30308, 30332, 30363]
     return zips
