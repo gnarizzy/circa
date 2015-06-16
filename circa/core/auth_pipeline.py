9bd98c678723cf65ea56bfa251fc3290e0ae1c4f
Andrew <aschuster3@gatech.edu>
2015-06-15 22:53:10 -0400

    Welcome email no longer errors for FB signups

--- circa/core/auth_pipeline.py
+++ circa/core/auth_pipeline.py
@@ -1,5 +1,9 @@
-from core.email import welcome_new_user_notification
+from core.email import welcome_new_user_fb_notification
 
 def send_welcome_email_social_auth(**kwargs):
-    if kwargs['is_new'] and kwargs['details']['email']:
-        welcome_new_user_notification(kwargs['details']['email'])
+    if kwargs['is_new']:
+        if kwargs['details']['email']:
+            welcome_new_user_fb_notification(kwargs['details']['username'], kwargs['details']['email'])
+        else:
+            pass
+            # panic
