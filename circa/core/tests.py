7cc8bfa2e3567938792ac83d1ca7123e94aa1a7e
Gautam <gautam@uga.edu>
2015-07-20 04:25:09 -0400

    test notes for slugs--too sleepy to implement myself

--- circa/core/tests.py
+++ circa/core/tests.py
@@ -492,3 +492,9 @@ class PromoTest(TestCase):
         self.assertEqual(form.errors, {
             'code': ['Sorry, that\'s not your code!']
         })
+
+
+#slug tests
+#1) correct id but incorrect slug redirects to correct slugified url (/listing/40/asdas redirects to /listing/40/actual-slug)
+#2) correct id but no slug redirects to slugified url, (/listing/40/ redirects to /listing/40/actual-slug)
+#3) correct slug--slug = slugify(item.title)(uses django.utils.text slugify method)
