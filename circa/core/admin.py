03a1deb54b12654445f347fd3621ca52c1d41a74
Gautam <gautam@uga.edu>
2015-08-05 00:42:52 -0400

    Registration form changes and addresses appear on admin site now

--- circa/core/admin.py
+++ circa/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import Item, Listing, UserProfile, PromoCode
+from core.models import Item, Listing, UserProfile, PromoCode, Address
 from image_cropping import ImageCroppingMixin
 
 class ItemAdmin(ImageCroppingMixin, admin.ModelAdmin):
@@ -9,3 +9,4 @@ admin.site.register(Item, ItemAdmin)
 admin.site.register(Listing)
 admin.site.register(UserProfile)
 admin.site.register(PromoCode)
+admin.site.register(Address)
